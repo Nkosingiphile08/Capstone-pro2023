1e4b2ba6b20852f0d06974bb40dd0c3cbd724498
SizoDevelops <fierceincii@gmail.com>
2023-10-02 10:10:42 +0200

    Add the my_flask_app folder and exclude things as necessary

---
+++ routes/profile.js
@@ -0,0 +1,82 @@
+const express = require("express")
+
+const router = express.Router()
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('./database/database.db');
+
+
+router.get("/login",  (req, res) => {
+   res.render("login")
+})
+router.get("/signup",  (req, res) => {
+   res.render("signup")
+})
+router.get("/appointment",  (req, res) => {
+    let doctors = [
+        { name: 'Dr One', address: '453 Doctor\'s street, Dr Town, Durban', phone: '031 245 7658' },
+        { name: 'Dr Two', address: '453 Doctor Ave, Dr City, Johannesburg', phone: '011 245 7658' },
+        { name: 'Dr Three', address: '453 DR lane, Doctorsburg, Cape Town', phone: '021 245 7658' },
+
+    ]
+   res.render("appointment", {doctors: doctors})
+})
+
+
+router.post("/login", (req, res) => {
+    db.get(`SELECT * FROM users WHERE email = ? AND password = ?`, [req.body.email, req.body.password], (err, row) => {
+        if (err) {
+            res.render("login", {user: null})
+        }
+        if (row) {
+          res.render("profile", {user: row})
+        } else {
+          res.render("login", {user: null})
+        }
+      });
+})
+
+
+router.post("/signup", (req, res) => {
+    db.run(`INSERT INTO users (name, email, contacts, password) VALUES (?, ?, ?, ?)`, [req.body.name, req.body.email, req.body.contacts, req.body.password], err => {
+        if(err){
+            console.log(err.message)
+            res.render("signup")
+        }
+        
+        else{
+           res.redirect("login")
+        }
+    })
+})
+
+
+
+router.post("/appointments", (req, res) => {
+    const data = {
+        time: req.body.time,
+        date: req.body.date,
+        doctor: req.body.doctor
+    }
+
+    const dataJSON = JSON.stringify(data)
+    
+    db.run(`UPDATE users SET appointments = ? WHERE email = ? `, [dataJSON, req.body.email], (err) => {
+        if(err) {
+            return console.error(err.message)
+        }
+        else {
+            db.get(`SELECT * FROM users WHERE email = ? `, [req.body.email], (err, row) => {
+                if (err) {
+                    res.render("appointment")
+                }
+                if (row) {
+                  res.render("profile", {user: row})
+                } else {
+                  res.render("appointment")
+                }
+              });
+        }
+    })
+})
+
+module.exports = router
---
+++ server.js
@@ -0,0 +1,62 @@
+const express = require("express")
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('./database/database.db');
+
+// Close the database connection when the Node.js process is terminated
+process.on('SIGINT', () => {
+  db.close((err) => {
+    if (err) {
+      console.error('Error closing the database:', err.message);
+    } else {
+      console.log('Database closed.');
+    }
+    process.exit(0);
+  });
+});
+
+const tableName = 'users';
+
+// Create the table if it doesn't exist
+db.run(`
+  CREATE TABLE IF NOT EXISTS ${tableName} (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    email TEXT UNIQUE,
+    contacts TEXT,
+    appointments TEXT,
+    password TEXT 
+  )
+`, (err) => {  
+  if (err) {
+    console.error('Error creating table:', err.message);
+  } else {
+    console.log(`Table '${tableName}' created or already exists.`);
+  }
+})
+
+  db.on('error', (err) => {
+    console.error('Database error:', err.message);
+  });
+
+  db.close((err) => {
+    if (err) {
+      console.error('Error closing the database:', err.message);
+    } else {
+      console.log('Database closed.');
+    }
+  }); 
+
+const app = express()
+
+const profileRouter = require("./routes/profile")
+
+app.set("view engine", 'ejs')
+app.use(express.urlencoded({extended: false}))
+app.use("/" , profileRouter)
+app.listen(3000)
+
+app.get("/", (req, res) => {
+    res.render("index")
+})
+
+module.exports = db
